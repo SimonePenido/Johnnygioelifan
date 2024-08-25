4aa8525eb4d94ca08d40368ce6493cd034896210
Simone Penido <simone_penido@hotmail.com>
2024-08-25 20:44:35 -0300

    update js gpdr

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-// script.js
-
-// Inicializa o Slick Carousel para rolagem de imagens, se necessário
 $(document).ready(function(){
-    $('.gallery').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
+    // Inicializa o slick-carousel
+    var $gallery = $('.gallery').slick({
+        dots: false,  // Oculta os botões indicadores (bolinhas)
+        autoplay: true,  // Habilita a rotação automática
+        autoplaySpeed: 2000,  // Tempo de espera entre cada slide (2 segundos)
+        prevArrow: $('.slick-prev'),
+        nextArrow: $('.slick-next')
     });
+
+    // Função para destruir o carousel em telas pequenas
+    function checkWidth() {
+        var windowSize = $(window).width();
+
+        if (windowSize <= 768) {
+            if ($gallery.hasClass('slick-initialized')) {
+                $gallery.slick('unslick'); // Desativa o carousel
+            }
+        } else {
+            if (!$gallery.hasClass('slick-initialized')) {
+                $gallery.slick({
+                    dots: false,  // Oculta os botões indicadores
+                    autoplay: true,
+                    autoplaySpeed: 2000,
+                    prevArrow: $('.slick-prev'),
+                    nextArrow: $('.slick-next')
+                });
+            }
+        }
+    }
+
+    // Verifica o tamanho da janela ao carregar a página
+    checkWidth();
+    
+    // Verifica o tamanho da janela ao redimensionar
+    $(window).resize(checkWidth);
 });
